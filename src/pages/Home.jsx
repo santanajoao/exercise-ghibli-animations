e90f46b5573043c815879d9f0a8f918a64256824
João Pedro Santana <jjoaopedrocostasantana@gmail.com>
2023-02-17 00:01:31 -0300

    style: adiciona estilização do loading

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import Header from '../components/Header';
 import Main from '../components/Main';
+import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div>
+    <div className={ styles.home }>
       <Header />
       <Main />
     </div>
